cae1b167d6c82006f8507fa9c5c305f52c8cae50
Faisa Saez <Family@Faisas-iMac.local>
2021-12-08 13:02:46 +0100

    without cache

---
+++ firstClass.py
@@ -0,0 +1,11 @@
+
+
+
+class class1:
+    x=5
+
+    def __init__(self, y):
+        self.y = y
+
+    def addCords(self):
+        print(self.x+self.y)
---
+++ main.py
@@ -0,0 +1,8 @@
+from firstClass import class1
+
+
+obj = class1(5)
+
+print(obj.x)
+
+obj.addCords()
