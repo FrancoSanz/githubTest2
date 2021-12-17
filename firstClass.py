bcb82b7b4c374201ab6247be46155dae65a5c4f9
FrancoSanz <22fsaezstudent@aloha-college.com>
2021-12-17 14:41:49 +0100

    edited addCords

--- firstClass.py
+++ firstClass.py
@@ -2,10 +2,10 @@
 
 
 class class1:
-    x=5
+    x=5 # joe mama value
 
     def __init__(self, y):
         self.y = y
 
     def addCords(self):
-        print(self.x+self.y)
+        print(self.x+self.y+10)
